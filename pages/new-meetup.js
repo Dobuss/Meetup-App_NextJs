f1a9bb8a3136f7fdc98695287928361b91e93847
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-29 17:29:28 +0200

    added mongoDB handler

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,11 +1,22 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-function NewMeetupPage(){
+function NewMeetupPage() {
+    const router = useRouter();
+  async function addMeetupHandler(enteredData) {
+    const response = await fetch('/api/new-meetup', {
+      method: "POST",
+      body: JSON.stringify(enteredData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-    function addMeetupHandler(enteredData){
-        //logic 
-    }
- return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    const data = await response.json();
+    console.log(data);
+    router.push('/');
+  }
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
 export default NewMeetupPage;
