eddd8d02c9ed28e91941f4d502d7647c2e3774d4
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-29 14:47:00 +0200

    showing the usage of getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import MeetupList from "../components/meetups/MeetupList";
 
+//SEO = search engine optimization
+
 const DUMMY_MEETUPS = [
   {
     id: "m1",
@@ -19,11 +21,22 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return (
-    <>
-      <MeetupList meetups={DUMMY_MEETUPS} />
-    </>
-  );
+function HomePage(props) {
+
+  return <MeetupList meetups={props.meetups} />;
 }
+
+export async function getStaticProps(){
+  // fetch data from an api
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 10 //regenerate the page after *10* seconds
+  };
+}
+// ^^ data is pre-rendered during the build process, not when the page is loaded
+// getStaticProps is a reserved name and it should always be used 
+// also the props parameter should always be called props
+
 export default HomePage;
